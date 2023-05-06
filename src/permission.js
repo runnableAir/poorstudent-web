edd2743a1b0a6dc086d2d1117216572f47095757
runnableAir <1316214297@qq.com>
2023-05-06 22:11:45 +0800

    fix(src/permission.js): 删除不必要的Message.error
    更改错误提示为控制台输出

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,5 @@
 import router from './router'
 import store from './store'
-import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
@@ -38,7 +37,7 @@ router.beforeEach(async(to, from, next) => {
         } catch (error) {
           // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
+          console.log('get user info failed! error:', error)
           next(`/login?redirect=${to.path}`)
           NProgress.done()
         }
